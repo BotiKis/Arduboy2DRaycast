19cfa5de6de5e17f6bc0b7b1049e9de6320f580b
Botond Kis <botond.kis@gmail.com>
2018-06-03 13:56:04 +0200

    ready to implement raycast

--- 2DRaycast/src/2DRaycast.ino
+++ 2DRaycast/src/2DRaycast.ino
@@ -3,11 +3,13 @@
 #include "MapTile.h"
 
 Arduboy2 arduboy;
+Sprites sprites;
 
 constexpr uint8_t MAPWIDTH  = 12;
 constexpr uint8_t MAPHEIGHT = 8;
+constexpr uint8_t TILESIZE = 8;
 
-uint8_t mapData[] PROGMEM =
+const uint8_t mapData[] PROGMEM =
 {
   0,0,0,0,0,0,0,0,0,0,0,0,
   0,1,0,0,0,0,0,0,0,0,0,0,
@@ -23,6 +25,10 @@ constexpr uint8_t playerSightDistance = 6;
 MapTile mapBuffer[MAPWIDTH*MAPHEIGHT];
 Point playerPos = {5,5};
 
+void drawMap();
+void updateMap();
+void clearMap();
+
 void setup() {
     // put your setup code here, to run once:
     arduboy.boot();
@@ -36,36 +42,99 @@ void setup() {
         uint8_t currentTileData = pgm_read_byte(mapData+x+y*MAPWIDTH);
 
         // populate mapbuffer
-        uint8_t currentTileInBuffer = mapBuffer[mapData+x+y*MAPWIDTH];
+        MapTile currentTileInBuffer = mapBuffer[x+y*MAPWIDTH];
+        currentTileInBuffer.tileID = currentTileData;
+        currentTileInBuffer.containsPlayer = 0;
+        currentTileInBuffer.drawFog = 1;
+        mapBuffer[x+y*MAPWIDTH] = currentTileInBuffer;
       }
     }
+
+    // set player start
+    mapBuffer[playerPos.x+playerPos.y*MAPWIDTH].containsPlayer = 1;
 }
 
 void loop() {
-    if(!arduboy.nextFrame()) continue;
+    if(!arduboy.nextFrame()) return;
+
+    Point tempPlayerPos = playerPos;
 
     // move player
     arduboy.pollButtons();
-    if (arduboy.justPressed(UP_BUTTON)){
-      playerPos.y--;
-    }
-    if (arduboy.justPressed(DOWN_BUTTON)){
-      playerPos.y++;
-    }
-    if (arduboy.justPressed(LEFT_BUTTON)){
-      playerPos.x--;
-    }
-    if (arduboy.justPressed(RIGHT_BUTTON)){
-      playerPos.x++;
+    if (arduboy.everyXFrames(8)) {
+      if (arduboy.pressed(UP_BUTTON)){
+        tempPlayerPos.y--;
+      }
+      if (arduboy.pressed(DOWN_BUTTON)){
+        tempPlayerPos.y++;
+      }
+      if (arduboy.pressed(LEFT_BUTTON)){
+        tempPlayerPos.x--;
+      }
+      if (arduboy.pressed(RIGHT_BUTTON)){
+        tempPlayerPos.x++;
+      }
+
+      // limit player pos
+      tempPlayerPos.x = min(tempPlayerPos.x, MAPWIDTH-1);
+      tempPlayerPos.x = max(tempPlayerPos.x, 0);
+      tempPlayerPos.y = min(tempPlayerPos.y, MAPHEIGHT-1);
+      tempPlayerPos.y = max(tempPlayerPos.y, 0);
+
+      // check if map needs to be updated
+      if (tempPlayerPos.x != playerPos.x || tempPlayerPos.y != playerPos.y) {
+        playerPos = tempPlayerPos;
+        updateMap();
+      }
     }
 
-    // limit player pos
-    playerPos.x = min(playerPos.x, MAPWIDTH-1);
-    playerPos.x = max(playerPos.x, 0);
-    playerPos.y = min(playerPos.y, MAPHEIGHT-1);
-    playerPos.y = max(playerPos.y, 0);
-
+    // do drawing
     arduboy.clear();
 
+    drawMap();
+
     arduboy.display();
+}
+
+void drawMap(){
+  static const uint8_t xOffset = 16;
+  for (uint8_t y = 0; y < MAPHEIGHT; y++) {
+    for (uint8_t x = 0; x < MAPWIDTH; x++) {
+      MapTile currentTileInBuffer = mapBuffer[x+y*MAPWIDTH];
+
+      // draw tiles
+      sprites.drawSelfMasked(x*TILESIZE+xOffset, y*TILESIZE, tilesheet, currentTileInBuffer.tileID);
+
+      // draw fog
+      if (currentTileInBuffer.drawFog == 1) {
+        sprites.drawErase(x*TILESIZE+xOffset, y*TILESIZE, tilesheet, TileFog);
+      }
+
+      // draw player
+      if (currentTileInBuffer.containsPlayer == 1) {
+        sprites.drawOverwrite(x*TILESIZE+xOffset, y*TILESIZE, tilesheet, TilePlayer);
+      }
+    }
+  }
+
+}
+
+void updateMap(){
+  // clear map
+  clearMap();
+
+  // update players pos
+  mapBuffer[playerPos.x+playerPos.y*MAPWIDTH].containsPlayer = 1;
+
+  // remove fog for player
+  // if i would just know how...
+}
+
+void clearMap(){
+  for (uint8_t y = 0; y < MAPHEIGHT; y++) {
+    for (uint8_t x = 0; x < MAPWIDTH; x++) {
+      mapBuffer[x+y*MAPWIDTH].drawFog = 1;
+      mapBuffer[x+y*MAPWIDTH].containsPlayer = 0;
+    }
+  }
 }
