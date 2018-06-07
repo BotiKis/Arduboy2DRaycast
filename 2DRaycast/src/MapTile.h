96df0e33fd89e7e144e12ce6ab8a722e2389ee9c
Botond Kis <botond.kis@gmail.com>
2018-06-07 23:11:59 +0200

    updated maptile to have a marker

--- 2DRaycast/src/MapTile.h
+++ 2DRaycast/src/MapTile.h
@@ -5,9 +5,10 @@
 
 class MapTile{
 public:
-  uint8_t tileID:1;   // 0... Plains, 1... Obstacle
-  uint8_t drawFog:1;  // 1... true, 0... false
-  uint8_t containsPlayer:1;  // 1... true, 0... false
+  uint8_t tileID:1;           // 0... Plains, 1... Obstacle
+  uint8_t drawFog:1;          // 1... true, 0... false
+  uint8_t drawMarker:1;       // 1... true, 0... false
+  uint8_t containsPlayer:1;   // 1... true, 0... false
 };
 
 #endif
